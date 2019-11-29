9870b850803fe5df0825a01317f81cea1c02ab16
Nguyen Le Huy Bao <nguyenlehb1808@gmail.com>
2019-11-29 17:19:45 +0700

    first commit

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,8 @@
 """
 Replace the contents of this module docstring with your own details
-Name:
-Date started:
-GitHub URL:
+Name:Nguyen le huy bao
+Date started:29/11/2019
+GitHub URL:https://github.com/JCUS-CP1404/assignment-01-aqua270120  
 """
 
 
